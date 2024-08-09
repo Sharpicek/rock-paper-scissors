22402092e37cfbdc8c49641be6d4ed89a386f793
Sharpicek <davidthomas13@proton.me>
2024-08-09 11:54:08 +0200

    Delete getHumanChoice, click calls playRound

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let roundCounter = 0;
+let roundCounter = 1;
 let humanScore = 0;
 let computerScore = 0;
 const paraChoice = document.createElement("p");
@@ -17,41 +17,22 @@ function getComputerChoice() {
             computerChoice = "scissors";
             break;
     }
+    return computerChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    console.log("Human choice: " + humanChoice);
     console.log("Computer choice: " + computerChoice);
     const container = document.querySelector("#score-container");
     paraChoice.textContent = `Round ${roundCounter}: Computer chose ${computerChoice}.`;
     container.appendChild(paraChoice);
-    return computerChoice;
-}
-
-function getHumanChoice(humanChoice) {
-    console.log("Human choice: " + humanChoice);
     ++roundCounter;
-    getComputerChoice();
-}
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        alert(`Your ${humanChoice} ties with computers ${computerChoice}.`);
-    } else if ((humanChoice == "rock" && computerChoice == "paper") || 
-            (humanChoice == "paper" && computerChoice == "scissors") || 
-            (humanChoice == "scissors" && computerChoice == "rock")) {
-        alert(`Your ${humanChoice} loses against computers ${computerChoice}.`);
-        ++computerScore;
-    } else if ((humanChoice == "rock" && computerChoice == "scissors") ||
-            (humanChoice == "paper" && computerChoice =="rock") ||
-            (humanChoice == "scissors" && computerChoice == "paper")) {
-        alert(`Your ${humanChoice} wins against computers ${computerChoice}.`);
-        ++humanScore;        
-    } else {}
-    console.log("Human score: " + humanScore);
-    console.log("Computer score: " + computerScore);
 }
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-rockButton.addEventListener("click", () => getHumanChoice("rock"));
-paperButton.addEventListener("click", () => getHumanChoice("paper"));
-scissorsButton.addEventListener("click", () => getHumanChoice("scissors"));
+rockButton.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperButton.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsButton.addEventListener("click", () => playRound("scissors", getComputerChoice()));
